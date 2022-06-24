35b8e5349555b427026aa515951420bc2388d194
Favour Nwajoko <chocorazzee@gmail.com>
2022-06-24 14:08:05 +0100

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,28 +1,48 @@
 import json
 import pickle
+import numpy as np
 
 
 __locations = None
 __data_columns = None
 __model = None
 
+def get_estimated_price(location, total_sqft, bath, bhk):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1    
+    x = np.zeros(len(__data_columns))
+    x[0] = total_sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index > 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0], 2)
+
+
 def get_location_names():
     return __locations
 
-def load_saved_artifacts():
-    print('Loading saved artifacts')
+def load_saved_tokens():
+    print('Loading saved tokens..... Start!!!')
     global __data_columns
     global __locations
 
-    with open(r"C:\Users\ifunanyaScript\Everything\House_price_prediction_repository\server\artifacts\columns.json", "r") as f:
+    with open(r"C:\Users\ifunanyaScript\Everything\House_price_prediction_repository\server\tokens\columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
     
-    with open(r"C:\Users\ifunanyaScript\Everything\House_price_prediction_repository\server\artifacts\house_prediction_model.pickle", "rb") as f:
+    global __model
+    with open(r"C:\Users\ifunanyaScript\Everything\House_price_prediction_repository\server\tokens\house_prediction_model.pickle", "rb") as f:
         __model = pickle.load(f)
+    print("Loading saved tokens...... Done!!!")
 
 if __name__ == "__main__":
-    load_saved_artifacts()
+    load_saved_tokens()
     print(get_location_names())
-
-# ifunanyaScript
+    print(get_estimated_price("WhiteField", 1500, 4, 4))
+    print(get_estimated_price("1st Phase JP Nagar", 1000, 3, 4))
+    print(get_estimated_price("Yelachenahalli", 2500, 8, 7))
+    print(get_estimated_price("Kalhalli", 1000, 2, 2))
