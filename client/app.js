949e444e074ad8aa88a01435e0b6dbf51c051400
Favour Nwajoko <chocorazzee@gmail.com>
2022-06-26 00:29:01 +0100

    Add files via upload

--- client/app.js
+++ client/app.js
@@ -1,3 +1,43 @@
+function getBathValue() {
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for(var i in uiBathrooms) {
+        if(uiBathrooms[i].checked) {
+            return parseInt(i)+1;
+        }
+    }
+    return -1;
+}
+
+function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for(var i in uiBHK) {
+        if(uiBHK[i].checked) {
+            return parseInt(i)+1;
+        }
+    }
+return -1;
+}
+
+function onClickedEstimatedPrice() {
+    console.log("Estimate price button clicked");
+    var total_sqft = document.getElementById("uiSqft");
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations");
+    var est_price = document.getElementById("uiEstimatedPrice");
+    var url = "http://127.0.0.1:5000/predict_home_price";
+
+    $.post(url, {
+        location: location.value,
+        total_sqft: parseFloat(total_sqft.value),
+        bath: bathrooms,
+        bhk: bhk
+    },function(data, status) {
+        console.log(data.estimated_price);
+        est_price.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+        console.log(status);
+    });
+}
 
 function onPageLoad() {
     console.log( "document loaded" );
@@ -17,3 +57,5 @@ function onPageLoad() {
 }
 
 window.onload = onPageLoad;
+
+//ifunanyaScript
