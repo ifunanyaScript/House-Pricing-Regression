d4af738cc2b5e6b5a753d9ba9ceed12fa0452287
Favour Nwajoko <chocorazzee@gmail.com>
2022-09-29 22:28:43 +0100

    server updated

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ import utils
 
 app = Flask(__name__)
 
-@app.route('/get_location_names')
+@app.route('/locations')
 def get_location_names():
     response = jsonify({
         'locations': utils.get_location_names()
@@ -12,15 +12,15 @@ def get_location_names():
 
     return response
 
-@app.route('/predict_home_price', methods=['POST'])
+@app.route('/house_pricing', methods=['POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
-    bhk = int(request.form['bhk'])
-    bath = int(request.form['bath'])
+    bedrooms = int(request.form['bedrooms'])
+    bathrooms = int(request.form['bathrooms'])
 
     response = jsonify({
-        'estimated_price': utils.get_estimated_price(location, total_sqft, bath, bhk)
+        'estimated_price': utils.get_estimated_price(location, total_sqft, bathrooms, bedrooms)
     })
 
     response.headers.add("Access-Control-Allow-Origin", "*")
