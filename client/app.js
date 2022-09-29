2ddf67068ca89c7e72f1c03537f3b02af5e70e12
Favour Nwajoko <chocorazzee@gmail.com>
2022-09-29 22:19:55 +0100

    script update

--- client/app.js
+++ client/app.js
@@ -1,17 +1,17 @@
-function getBathValue() {
-    var uiBathrooms = document.getElementsByName("uiBathrooms");
-    for(var i in uiBathrooms) {
-        if(uiBathrooms[i].checked) {
+function getBathroomValue() {
+    var bathrooms = document.getElementsByName("bathrooms");
+    for(var i in bathrooms) {
+        if(bathrooms[i].checked) {
             return parseInt(i)+1;
         }
     }
     return -1;
 }
 
-function getBHKValue() {
-    var uiBHK = document.getElementsByName("uiBHK");
-    for(var i in uiBHK) {
-        if(uiBHK[i].checked) {
+function getBedroomValue() {
+    var bedrooms = document.getElementsByName("bedrooms");
+    for(var i in bedrooms) {
+        if(bedrooms[i].checked) {
             return parseInt(i)+1;
         }
     }
@@ -20,18 +20,18 @@ return -1;
 
 function onClickedEstimatedPrice() {
     console.log("Estimate price button clicked");
-    var total_sqft = document.getElementById("uiSqft");
-    var bhk = getBHKValue();
-    var bathrooms = getBathValue();
-    var location = document.getElementById("uiLocations");
-    var est_price = document.getElementById("uiEstimatedPrice");
-    //var url = "http://127.0.0.1:5000/predict_home_price";
-    var url = "/api/predict_home_price";
+    var total_sqft = document.getElementById("area_sqft");
+    var bedrooms = getBedroomValue();
+    var bathrooms = getBathroomValue();
+    var location = document.getElementById("locations");
+    var est_price = document.getElementById("price");
+    var url = "http://127.0.0.1:5000/house_pricing";
+    // var url = "/api/predict_home_price";
     $.post(url, {
         location: location.value,
         total_sqft: parseFloat(total_sqft.value),
-        bath: bathrooms,
-        bhk: bhk
+        bathrooms: bathrooms,
+        bedrooms: bedrooms
     },function(data, status) {
         console.log(data.estimated_price);
         est_price.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
@@ -40,18 +40,20 @@ function onClickedEstimatedPrice() {
 }
 
 function onPageLoad() {
-    console.log( "document loaded" );
-    //var url = "http://127.0.0.1:5000/get_location_names";
-    var url = "/api/get_location_names";
+    console.log( "locations loaded" );
+
+    // Use a url of your choice by uncommenting as you please.
+    var url = "http://127.0.0.1:5000/locations";
+    // var url = "/api/get_location_names";
     $.get(url, function(data, status) {
         console.log("got response for get_location_names request");
         if(data) {
             var locations = data.locations;
-            var uiLocations = document.getElementById("uiLocations");
-            $("#uiLocations").empty();
+            var uiLocations = document.getElementById("locations");
+            $("#locations").empty();
             for(var i in locations) {
                 var opt = new Option(locations[i]);
-                $('#uiLocations').append(opt);
+                $('#locations').append(opt);
             }
         }
     })
